71b0a582f39277b8505910d5f6ef18aea1bc8eac
Aaron <FlashBanistan66@gmail.com>
2017-08-22 18:42:44 -0600

    add serializer

--- tenants/serializers.py
+++ tenants/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework.serializers import (
+    HyperlinkedModelSerializer,
     ModelSerializer,
     ChoiceField,
-    SerializerMethodField
+    SerializerMethodField,
+    HyperlinkedRelatedField,
 )
-from .models import Tenant
+from .models import (
+    Tenant,
+    OccupantType,
+)
 
-class TenantSerializer(ModelSerializer):
+class OccupantTypeSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = OccupantType
+        fields = '__all__'
+
+class TenantSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Tenant
-        fields = '__all__'
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'ssn',
+            'occupant_type',
+            'unit'
+        ]
