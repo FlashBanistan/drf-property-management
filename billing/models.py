5c44504e84b60f45a8631880f36705621d14c2e5
Aaron <FlashBanistan66@gmail.com>
2017-09-19 21:58:55 -0600

    create initial Invoice model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from legal.models import Lease
+from authentication.models import Tenant
 
-# Create your models here.
+
+class Invoice(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    # Relationships:
+    lease = models.ForeignKey(Lease)
+    tenant = models.ForeignKey(Tenant)
