f3a05c3675c2501319b7d2f62495a30d7a335d13
Aaron <FlashBanistan66@gmail.com>
2017-09-21 20:31:19 -0600

    work on invoice model

--- billing/models.py
+++ billing/models.py
@@ -4,8 +4,9 @@ from authentication.models import Tenant
 
 
 class Invoice(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
     # Relationships:
     lease = models.ForeignKey(Lease)
     tenant = models.ForeignKey(Tenant)
+
+class Charge(models.Model):
+    invoice = models.ForeinKey
