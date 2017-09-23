f96aa6a94871aa069305db308bb3aa37fe6bc680
Aaron <FlashBanistan66@gmail.com>
2017-09-23 16:24:01 -0600

    create views

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import ChargeSerializer, PaymentSerializer
+from .models import Charge, Payment
 
-# Create your views here.
+
+class ChargeViewSet(viewsets.ModelViewset):
+    queryset = Charge.objects.all()
+    serializer_class = ChargeSerializer
+    filter_fields = '__all__'
+    ordering_fields = '__all__'
+    search_fields = (
+        'id',
+        'name'
+    )
+
+class PaymentViewSet(viewsets.ModelViewset):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_fields = '__all__'
+    ordering_fields = '__all__'
+    search_fields = (
+        'id'
+    )
